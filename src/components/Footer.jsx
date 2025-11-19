c11e4918325db895282e06bde0cb06018fe9bbce
ivanbaradi <ivanbaradi@gmail.com>
2025-11-19 10:47:56 -0800

    edited footer and body

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 function Footer(){
 
+    const curr_year = new Date().getFullYear()
+    const start_year = 2025
+    const copyright_years = (curr_year - start_year > 0) ? `${start_year} - ${curr_year}` : start_year
+
+    return <footer>Copyright &copy; {copyright_years} by Ivan Baradi</footer>
 }
 
 export default Footer
