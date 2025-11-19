eeec9813dc8858913886394758acd5e647023e75
ivanbaradi <ivanbaradi@gmail.com>
2025-11-19 13:50:03 -0800

    properly indented HTML tags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,25 +13,25 @@ function Home(){
             <div className="sub-content">
                 <h2 style={title}>What Can You Do Here?</h2>
                 <div className="container">
-                <div className="row">
-                    <FeatureInfo 
-                        subtitle='➕ Add Item'
-                        desc='Add items to the list you need for shopping, errands, etc.'
-                    />
-                    <FeatureInfo 
-                        subtitle='📝 Edit Item'
-                        desc='Made a mistake? Never too late to change! We got you!'
-                    />
-                    <FeatureInfo 
-                        subtitle='✅ Check Item'
-                        desc="Don't forget to cross the item once you have it!"
-                    />
-                    <FeatureInfo 
-                        subtitle='🗑️ Delete Item'
-                        desc='Done for your item for good? Might as well just throw it away! Otherwise, just cross it.'
-                    />
-                </div>
-            </div>                
+                    <div className="row">
+                        <FeatureInfo 
+                            subtitle='➕ Add Item'
+                            desc='Add items to the list you need for shopping, errands, etc.'
+                        />
+                        <FeatureInfo 
+                            subtitle='📝 Edit Item'
+                            desc='Made a mistake? Never too late to change! We got you!'
+                        />
+                        <FeatureInfo 
+                            subtitle='✅ Check Item'
+                            desc="Don't forget to cross the item once you have it!"
+                        />
+                        <FeatureInfo 
+                            subtitle='🗑️ Delete Item'
+                            desc='Done for your item for good? Might as well just throw it away! Otherwise, just cross it.'
+                        />
+                    </div>
+                </div>                
             </div>
         </div>
     )
