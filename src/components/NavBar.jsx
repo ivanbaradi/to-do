ee31fd510c994e6fc88245c4cea9acb67d8367de
ivanbaradi <ivanbaradi@gmail.com>
2025-11-21 12:23:09 -0800

    added active states of navbar items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar(){
 
-    console.log('navbar activated')
+    // useState to change navbar active item states by name
+    const [activeNavItem, setActiveNavItem] = useState(null)
 
-    const navHeader = {
-
+    /** 
+     * Changes states navbar active item when clicked
+     * @param {string} itemName - name of the navbar item
+     */
+    function changeActiveNavItem(itemName){
+        setActiveNavItem(itemName)
     }
 
     const navItem = {
@@ -15,7 +21,7 @@ function NavBar(){
     return (
         <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
             <div className='container-fluid'>
-                <Link to='/' className='navbar-brand h1 mb-0'>To Do</Link>
+                <Link className='navbar-brand h1 mb-0' onClick={changeActiveNavItem} to='/'>To Do</Link>
                 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -23,9 +29,9 @@ function NavBar(){
                 
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <div className="navbar-nav">
-                        <Link className="nav-link" style={navItem} to='/add-item'>Add Item</Link>
-                        <Link className="nav-link" style={navItem} to='/list'>List</Link>
-                        <Link className="nav-link" style={navItem} to='/about'>About</Link>
+                        <Link className={activeNavItem === 'item1' ? 'nav-link active' : 'nav-link'} style={navItem} onClick={() => changeActiveNavItem('item1')} to='/add-item'>Add Item</Link>
+                        <Link className={activeNavItem === 'item2' ? 'nav-link active' : 'nav-link'} style={navItem} onClick={() => changeActiveNavItem('item2')} to='/list'>List</Link>
+                        <Link className={activeNavItem === 'item3' ? 'nav-link active' : 'nav-link'} style={navItem} onClick={() => changeActiveNavItem('item3')} to='/about'>About</Link>
                     </div>
                 </div>
             </div>
