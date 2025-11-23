94aacff10cde2f465dc03648a22b0b9785bed5f8
ivanbaradi <ivanbaradi@gmail.com>
2025-11-23 02:20:04 -0800

    renamed featureQuery to featureMarginQuery

--- src/components/FeatureInfo.jsx
+++ src/components/FeatureInfo.jsx
@@ -24,11 +24,11 @@ export default function FeatureInfo({iconName, subTitle, desc, mobileMarginAdjus
         marginBottom: '20px'
     }
 
-    // media responsiveness for dynamic CSS changing on features (mobiles and tablets)
-    const featureQuery = onMobile ? mobileMarginAdjust && {marginBottom: '40px'} : onTablet && tabletMarginAdjust && {marginBottom: '80px'}
+    // media responsiveness to dynamically change margins for mobiles and tablets
+    const featureMarginQuery = onMobile ? mobileMarginAdjust && {marginBottom: '40px'} : onTablet && tabletMarginAdjust && {marginBottom: '80px'}
 
     return (
-        <div className="col-12 col-md-6 col-lg-3" style={{...feature, ...featureQuery}}>
+        <div className="col-12 col-md-6 col-lg-3" style={{...feature, ...featureMarginQuery}}>
             <i className='material-symbols-outlined' style={icon}>{iconName}</i>
             <h3 style={_subTitle}>{subTitle}</h3>
             <p style={{textAlign: !onMobile && 'left'}}>{desc}</p> {/* text aligns leftwards when on tablets and laptops */}
