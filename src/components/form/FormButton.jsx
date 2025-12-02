5a6462e6f33117d926e8a3fe98ee5c4823b376c8
ivanbaradi <ivanbaradi@gmail.com>
2025-12-02 13:31:20 -0800

    added media query

--- src/components/form/FormButton.jsx
+++ src/components/form/FormButton.jsx
@@ -1,14 +1,17 @@
-export default function FormButton({text, onClickEvent}){
+import { useMediaQuery } from "react-responsive"
+export default function FormButton({text, onClickEvent, mobileMarginAdjust}){
+
+    const onMobile = useMediaQuery({maxWidth: 767})
     
     return (
-        <div className="col-6">
-            <button 
-                className="btn btn-dark" 
+        <div className="col-md-6" style={{marginBottom: onMobile && mobileMarginAdjust && '20px'}}>
+            <input 
+                className="btn btn-dark"
+                type="button" 
                 style={{padding: '5px 20px'}} 
-                onClick={event => {
-                    event.preventDefault()
-                    onClickEvent()
-            }}>{text}</button>
+                onClick={() => onClickEvent()} 
+                value={text}
+            />
         </div>
     )
 }
