a55fc27d72a6ab6b76d7c99aebce7abbb03b827c
ivanbaradi <ivanbaradi@gmail.com>
2026-02-05 00:33:57 -0800

    made all buttons stacked for all devices

--- src/components/item/ItemButton.jsx
+++ src/components/item/ItemButton.jsx
@@ -2,14 +2,12 @@ import { useMediaQuery } from "react-responsive"
 
 export default function ItemButton({buttonVariant, iconName, text, func, id}){
 
-    const onMobile = useMediaQuery({maxWidth: 767})
-
     const button = {
         display: 'inline-flex',
         justifyContent: 'center',
         gap: '10px',
         width: '100%',
-        margin: !onMobile && iconName === 'check_box' && '2px 0', // middle button only
+        margin: iconName === 'check_box' && '2px 0', // middle button only
         borderRadius: 0
     }
 
@@ -19,7 +17,7 @@ export default function ItemButton({buttonVariant, iconName, text, func, id}){
     }
 
     return (
-        <div className="col-4 col-md-12">
+        <div className="col-12">
             <button className={`btn ${buttonVariant}`} style={button} onClick={() => {if(func !== undefined){func(id)}}}>
                 <i className="material-symbols-outlined">{iconName}</i>
                 <span style={textCSS}>{text}</span>
