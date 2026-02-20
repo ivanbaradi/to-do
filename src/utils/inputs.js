8ba1943516d4db9dfdd1abcec4915ffe278883ce
ivanbaradi <ivanbaradi@gmail.com>
2026-02-20 11:06:13 -0800

    edited documentations

--- src/utils/inputs.js
+++ src/utils/inputs.js
@@ -1,8 +1,8 @@
 /**
- * Sets input values from the form
+ * Sets an input 
  * @param {React.ChangeEvent<HTMLInputElement>} event - triggered to set text
  * @param {number} charLimit - character limit of an input
- * @param {React.Dispatch<React.SetStateAction<string>>} setText - setState of an input
+ * @param {React.Dispatch<React.SetStateAction<string>>} setText - input's setState
  */
 export function setInput(event, charLimit, setText){
     const {value} = event.target
@@ -10,8 +10,8 @@ export function setInput(event, charLimit, setText){
 }
 
 /**
- * 
- * @param {React.Dispatch<React.SetStateAction<string>>} setText - setState of an input
+ * Clears an input
+ * @param {React.Dispatch<React.SetStateAction<string>>} setText - input's setState
  */
 export function clearInput(setText){
     setText('')
